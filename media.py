5825c76b435506d07546125add6c6028211bcc04
MikeNight <ibigcheese91@yahoo.com>
2016-12-27 20:19:51 -0500

    Update media.py

--- media.py
+++ media.py
@@ -7,7 +7,7 @@ class Movie():
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
-    """ This method compiles the list when called """
+        """ This method compiles the list when called """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
